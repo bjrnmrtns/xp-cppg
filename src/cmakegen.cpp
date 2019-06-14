0395d474734823a12f8ecf3305e973d06a4667ee
Bjorn Martens <bjorn@expeditious.nl>
2019-06-14 22:53:42 +0200

    reading whole file

--- src/cmakegen.cpp
+++ src/cmakegen.cpp
@@ -1,6 +1,21 @@
-#include <boost/filesystem.hpp>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <optional>
+
+namespace {
+    std::string read_file(const std::string& file_name) {
+        std::ifstream ifs(file_name, std::ios::in | std::ios::binary);
+        std::ifstream::pos_type file_size = ifs.tellg();
+        ifs.seekg(0, std::ios::beg);
+        std::vector<char> file_contents(file_size);
+        ifs.read(file_contents.data(), file_size); 
+        return std::string(file_contents.data(), file_size);
+    }
+}
 
 int main()
 {
+    std::string tree = read_file("tree");
     return 0;
 }
