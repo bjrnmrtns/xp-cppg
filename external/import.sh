4da756deeeccd8d327deff1304aadd9b80ceb950
Bjorn Martens <bjorn@expeditious.nl>
2019-12-11 19:27:08 +0100

    changed import script

--- external/import.sh
+++ external/import.sh
@@ -2,3 +2,6 @@
 git clone --depth 1 https://github.com/abseil/abseil-cpp.git
 rm -rf abseil-cpp/.git
 rm -rf abseil-cpp/.gitignore
+https://github.com/tomtom-international/cpp-dependencies.git
+rm -rf cpp-dependencies/.git
+rm -rf cpp-dependencies/.gitignore
